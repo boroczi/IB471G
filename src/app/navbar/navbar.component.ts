e99d47b26755a582a04a09ace6ebf5e57c3277d0
Böröczi Patrik <boroczi2003@gmail.com>
2025-04-04 16:31:24 +0200

    feat: refactor navbar component to use dynamic navigation items and improve styles

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import {Component, ViewChild} from '@angular/core';
+import {RouterLink} from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSidenav, MatSidenavModule} from '@angular/material/sidenav';
-import {RouterLink} from '@angular/router';
 import {MatListItem, MatNavList} from '@angular/material/list';
+import {INavItem} from '../shared/interface/navitem.interface';
 
 @Component({
   selector: 'app-navbar',
@@ -24,9 +25,20 @@ import {MatListItem, MatNavList} from '@angular/material/list';
 export class NavbarComponent {
   @ViewChild('sidenav') sidenav: MatSidenav | undefined;
 
-  toggleSidenav() {
+  toggle() : void {
     if (this.sidenav) {
       this.sidenav.toggle();
     }
   }
+
+  protected readonly navItems = navItems;
 }
+
+const navItems : INavItem[] = [
+  {label: '< Vissza', role: 'user', mobileOnly: true},
+  {label: 'Home', role: 'user', routerLink: '/'},
+  {label: 'Tickets', role: 'user', routerLink: '/tickets'},
+  {label: 'Users', role: 'user', routerLink: '/users'},
+  {label: 'Settings', role: 'user', routerLink: '/settings'},
+  {label: 'Logout', role: 'user', routerLink: '/logout'}
+];
---
+++ src/app/shared/interface/navitem.interface.ts
@@ -0,0 +1,9 @@
+import {Role} from './user.interface';
+
+export interface INavItem {
+  label: string;
+  role: Role;
+  routerLink?: string;
+  mobileOnly?: boolean;
+  icon?: string;
+}
