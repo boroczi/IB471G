8fc9a3e0c81522da05bac32acaba4d08df00fc3c
Böröczi Patrik <boroczi2003@gmail.com>
2025-04-04 16:31:02 +0200

    feat: update SnackbarService to remove action parameter and enhance styles

--- src/app/shared/service/snackbar.service.ts
+++ src/app/shared/service/snackbar.service.ts
@@ -4,11 +4,12 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 @Injectable({
   providedIn: 'root'
 })
+
 export class SnackbarService {
   constructor(private snackbar: MatSnackBar) { }
 
-  open(message: string, action: string = 'Close', duration: number = 3000): void {
-    this.snackbar.open(message, action, {
+  open(message: string, duration: number = 3000): void {
+    this.snackbar.open(message, '', {
       duration: duration,
       panelClass: ['snackbar']
     });
