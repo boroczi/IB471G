0c43c731f5c132ed69b0751519580b7712c86c4f
Böröczi Patrik <boroczi2003@gmail.com>
2025-04-04 02:10:49 +0200

    feat: implement responsive navbar with Angular Material and update app structure

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, AfterViewInit, ViewChild} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
