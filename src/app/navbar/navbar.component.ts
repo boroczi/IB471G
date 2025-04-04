6e330d05657c2166df9a84e2e8cd9b7e03805779
Böröczi Patrik <boroczi2003@gmail.com>
2025-04-04 02:10:21 +0200

    feat: implement responsive navbar with Angular Material and update app structure

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, Input, AfterViewInit, ViewChild} from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenav, MatSidenavModule} from '@angular/material/sidenav';
+import {RouterLink} from '@angular/router';
+import {MatListItem, MatNavList} from '@angular/material/list';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
+  imports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterLink,
+    MatNavList,
+    MatListItem
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
 
+export class NavbarComponent {
+  @ViewChild('sidenav') sidenav: MatSidenav | undefined;
+
+  toggleSidenav() {
+    if (this.sidenav) {
+      this.sidenav.toggle();
+    }
+  }
 }
