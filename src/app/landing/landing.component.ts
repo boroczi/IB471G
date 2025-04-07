85a98b68d5d190662a68f73d38b325633fbde889
Böröczi Patrik <boroczi2003@gmail.com>
2025-04-07 17:24:41 +0200

    feat: enhance landing component with new card layout and styles

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatFabButton} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-landing',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatIconModule,
+    MatFabButton,
+    RouterLink
+  ],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
 export class LandingComponent {
-
 }
