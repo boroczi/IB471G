3ccd5ae81e7fcd0ff326ef64af7138639b1cb7f3
Böröczi Patrik <boroczi2003@gmail.com>
2025-04-07 17:25:11 +0200

    feat: add new routes for admin, auth, checkout, events, and profile components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,46 @@ export const routes: Routes = [
     pathMatch: 'full'
   },
   {
+    path: 'admin',
+    loadComponent: () => import('./admin/admin.component').then(m => m.AdminComponent),
+    pathMatch: 'full',
+  },
+  {
+    path: 'auth',
+    loadComponent: () => import('./auth/auth.component').then(m => m.AuthComponent),
+    pathMatch: 'full',
+  },
+  {
+    path: 'auth',
+    children: [
+      {
+        path: 'login',
+        loadComponent: () => import('./auth/login/login.component').then(m => m.LoginComponent),
+        pathMatch: 'full'
+      },
+      {
+        path: 'signup',
+        loadComponent: () => import('./auth/signup/signup.component').then(m => m.SignupComponent),
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: 'checkout',
+    loadComponent: () => import('./checkout/checkout.component').then(m => m.CheckoutComponent),
+    pathMatch: 'full',
+  },
+  {
+    path: 'events',
+    loadComponent: () => import('./events/events.component').then(m => m.EventsComponent),
+    pathMatch: 'full',
+  },
+  {
+    path: 'profile',
+    loadComponent: () => import('./profile/profile.component').then(m => m.ProfileComponent),
+    pathMatch: 'full',
+  },
+  {
     path: '**',
     redirectTo: ''
   },
