6603ae9cfb80f154501b24e5420f787d40a553fa
Böröczi Patrik <boroczi2003@gmail.com>
2025-04-08 14:53:55 +0200

    feat: create EventsComponent with event listing and details

---
+++ src/app/events/events.component.ts
@@ -0,0 +1,61 @@
+import {Component} from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatTableModule} from '@angular/material/table';
+import {MatIconModule} from '@angular/material/icon';
+import {IEvent} from '../shared/interface/event.interface';
+import {DatePipe} from '../shared/pipe/date.pipe';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatFabButton} from '@angular/material/button';
+
+@Component({
+  selector: 'app-events',
+    imports: [
+        MatCardModule,
+        MatTableModule,
+        MatIconModule,
+        DatePipe,
+        MatExpansionModule,
+        MatFabButton
+    ],
+  templateUrl: './events.component.html',
+  styleUrl: './events.component.scss'
+})
+export class EventsComponent {
+  events: IEvent[] = [
+    {
+      id: 1,
+      name: 'Koncert A',
+      location: 'IH',
+      startTime: new Date('2025-04-01T19:00:00'),
+      endTime: new Date('2025-04-01T22:00:00'),
+      description: 'A great concert featuring popular bands.',
+      totalTickets: 500,
+      availableTickets: 200,
+      ticketPrice: 50
+    },
+    {
+      id: 2,
+      name: 'Koncert B',
+      location: 'Budapest Park',
+      startTime: new Date('2025-05-15T12:00:00'),
+      endTime: new Date('2025-05-15T23:00:00'),
+      description: 'An all-day festival with food, music, and fun activities.',
+      totalTickets: 1000,
+      availableTickets: 750,
+      ticketPrice: 30
+    },
+    {
+      id: 3,
+      name: 'Koncert C',
+      location: 'MVM Dome',
+      startTime: new Date('2025-06-20T18:00:00'),
+      endTime: new Date('2025-06-20T21:00:00'),
+      description: 'A captivating play performed by renowned actors.',
+      totalTickets: 300,
+      availableTickets: 50,
+      ticketPrice: 40
+    }
+  ];
+
+  protected readonly DatePipe = DatePipe;
+}
