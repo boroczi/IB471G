2092c9950c1f6153ed457402f6eaedddfb4e4658
Böröczi Patrik <boroczi2003@gmail.com>
2025-04-13 18:23:58 +0200

    refactor: update import path for NavbarComponent in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {NavbarComponent} from './navbar/navbar.component';
+import {NavbarComponent} from './shared/component/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
