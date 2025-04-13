48c413e2390cea60fcd3c1ca50425dced31a74e2
Böröczi Patrik <boroczi2003@gmail.com>
2025-04-13 18:24:16 +0200

    refactor: update import path for NavbarComponent in app component

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,34 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from '@angular/material/card';
+import {MatFabButton, MatIconButton} from '@angular/material/button';
+import {EAuth} from '../shared/enum/auth.enum';
+import {MatIcon} from '@angular/material/icon';
+
+@Component({
+  selector: 'app-auth',
+  imports: [
+    MatCard,
+    MatCardContent,
+    MatFabButton,
+    MatCardHeader,
+    MatIcon,
+    MatIconButton
+  ],
+  templateUrl: './auth.component.html',
+  styleUrl: './auth.component.scss'
+})
+export class AuthComponent {
+  @Output() modal: EventEmitter<EAuth> = new EventEmitter<EAuth>();
+  @Input() closeModal!: () => void;
+
+  changeModal(modal: EAuth): void {
+    this.modal.emit(modal);
+  }
+
+  close(): void {
+    this.closeModal();
+  }
+
+  protected readonly EAuth = EAuth;
+}
+
