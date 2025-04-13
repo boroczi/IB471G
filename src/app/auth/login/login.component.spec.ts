8f764ff7f9d156c49af2623ec1616df53e5da90b
Böröczi Patrik <boroczi2003@gmail.com>
2025-04-13 18:25:29 +0200

    feat: add login component with form and authentication logic

---
+++ src/app/auth/login/login.component.spec.ts
@@ -0,0 +1,35 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+import { LoginComponent } from './login.component';
+
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        ReactiveFormsModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule
+      ],
+      declarations: [LoginComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,59 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormField, MatInput, MatLabel} from '@angular/material/input';
+import {MatButton, MatIconButton} from '@angular/material/button';
+import {SnackbarService} from '../../shared/service/snackbar.service';
+import {IUser} from '../../shared/interface/user.interface';
+import {MatIcon} from '@angular/material/icon';
+import {EAuth} from '../../shared/enum/auth.enum';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  imports: [
+    MatCardModule,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatFormField,
+    MatButton,
+    ReactiveFormsModule,
+    MatIcon,
+    MatIconButton
+  ],
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+  @Output() user: EventEmitter<Partial<IUser>> = new EventEmitter<Partial<IUser>>();
+  @Input() changeModal!: (modal: EAuth) => void;
+
+  constructor(private fb: FormBuilder, private SnackBar: SnackbarService) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.get('username')?.value == "test" && this.loginForm.get('password')?.value == "test") {
+      this.user.emit(
+        {
+          "id": 1,
+          "username": "test",
+          "email": "test@test.com",
+          "firstName": "Test",
+          "lastName": "User",
+          "role": "admin"
+        });
+      this.SnackBar.open("Sikeres bejelentkezés");
+    } else {
+      this.SnackBar.open("Sikertelen bejelentkezés");
+    }
+  }
+
+  closeLogin(): void {
+    this.changeModal(EAuth.auth);
+  }
+}
