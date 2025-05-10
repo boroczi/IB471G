b58a34fa1bbe23700442521534e6b3c42547b047
Böröczi Patrik <boroczi2003@gmail.com>
2025-05-11 00:04:43 +0200

    fix: update DatePipe to handle Timestamp and improve error handling

--- src/app/shared/pipe/date.pipe.ts
+++ src/app/shared/pipe/date.pipe.ts
@@ -1,26 +1,32 @@
-import {Pipe, PipeTransform} from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
+import { Timestamp } from '@angular/fire/firestore';
 
 @Pipe({
   name: 'date',
 })
 export class DatePipe implements PipeTransform {
-
-  transform(value: Date): string {
+  transform(value: Timestamp | Date): string {
     if (!value) {
-      return "A dátum nem elérhető!";
+      return 'A dátum nem elérhető!';
+    }
+
+    let date: Date;
+    if (value instanceof Timestamp) {
+      date = value.toDate();
+    } else {
+      date = value;
     }
 
     try {
-      const year = value.getFullYear();
-      const month = ('0' + (value.getMonth() + 1)).slice(-2);
-      const day = ('0' + value.getDate()).slice(-2);
-      const hours = ('0' + value.getHours()).slice(-2);
-      const minutes = ('0' + value.getMinutes()).slice(-2);
+      const year = date.getFullYear();
+      const month = ('0' + (date.getMonth() + 1)).slice(-2);
+      const day = ('0' + date.getDate()).slice(-2);
+      const hours = ('0' + date.getHours()).slice(-2);
+      const minutes = ('0' + date.getMinutes()).slice(-2);
 
       return `${year}.${month}.${day}. ${hours}:${minutes}`;
     } catch (e) {
-      return "A dátum nem elérhető!";
+      return 'A dátum nem elérhető!';
     }
   }
-
 }
