48b219deabc9074d720b6f4f4ecc682e7d2ca972
Böröczi Patrik <boroczi2003@gmail.com>
2025-05-11 00:25:53 +0200

    feat: implement event service with CRUD operations

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,12 +1,13 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatTableModule} from '@angular/material/table';
 import {MatIconModule} from '@angular/material/icon';
 import {DatePipe} from '../shared/pipe/date.pipe';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {MatFabButton} from '@angular/material/button';
-import {events} from '../shared/constant/events.const';
 import {SnackbarService} from '../shared/service/snackbar.service';
+import { EventService } from '../shared/service/event.service';
+import { IEvent } from '../shared/interface/event.interface';
 
 @Component({
   selector: 'app-events',
@@ -21,36 +22,42 @@ import {SnackbarService} from '../shared/service/snackbar.service';
   templateUrl: './events.component.html',
   styleUrl: './events.component.scss'
 })
-export class EventsComponent {
+export class EventsComponent implements OnInit {
   protected readonly DatePipe = DatePipe;
-  protected readonly events = events;
+  events: IEvent[] = [];
 
-  constructor(private SnackBar: SnackbarService) { }
+  constructor(private SnackBar: SnackbarService, private eventService: EventService) { }
 
-  addToCart(id: number, name: string, price: number) {
-  if (localStorage.getItem('isLoggedIn') == 'true') {
-    const cart = localStorage.getItem('cart');
-
-    if (cart) {
-      const cartObject = JSON.parse(cart);
-
-      if (cartObject[id]?.quantity) {
-        cartObject[id].quantity++;
-      } else {
-        cartObject[id] = { name, price, quantity: 1 };
-      }
-      localStorage.setItem('cart', JSON.stringify(cartObject));
-      console.log(this.localStorage.getItem('cart'));
-    } else {
-      const newCart: { [key: number]: { name: string; price: number; quantity: number } } = {};
-      newCart[id] = { name, price, quantity: 1 };
-      localStorage.setItem('cart', JSON.stringify(newCart));
-      console.log(this.localStorage.getItem('cart'));
-    }
-  } else {
-    this.SnackBar.open("Kérlek jelentkezz be a vásárláshoz");
+  ngOnInit(): void {
+    this.eventService.getEvents().subscribe((events) => {
+      this.events = events;
+    });
   }
-}
+
+  addToCart(id: string, name: string, price: number) {
+    if (localStorage.getItem('isLoggedIn') == 'true') {
+      const cart = localStorage.getItem('cart');
+
+      if (cart) {
+        const cartObject = JSON.parse(cart);
+
+        if (cartObject[id]?.quantity) {
+          cartObject[id].quantity++;
+        } else {
+          cartObject[id] = { name, price, quantity: 1 };
+        }
+        localStorage.setItem('cart', JSON.stringify(cartObject));
+        console.log(this.localStorage.getItem('cart'));
+      } else {
+        const newCart: { [key: string]: { name: string; price: number; quantity: number } } = {};
+        newCart[id] = { name, price, quantity: 1 };
+        localStorage.setItem('cart', JSON.stringify(newCart));
+        console.log(this.localStorage.getItem('cart'));
+      }
+    } else {
+      this.SnackBar.open("Kérlek jelentkezz be a vásárláshoz");
+    }
+  }
 
   nuhuh() {
     this.SnackBar.open("¯\\_( ͡° ͜ʖ ͡°)_/¯");
--- src/app/shared/service/event.service.ts
+++ src/app/shared/service/event.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Firestore, collection, doc, setDoc, collectionData, deleteDoc, updateDoc } from '@angular/fire/firestore';
+import { IEvent } from '../interface/event.interface';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventService {
 
-  constructor() { }
+  constructor(private firestore: Firestore) { }
+
+  async createEvent(event: Partial<IEvent>): Promise<void> {
+    const userRef = doc(collection(this.firestore, 'events'));
+    setDoc(userRef, event);
+  }
+
+  getEvents(): Observable<IEvent[]> {
+    const eventsCollection = collection(this.firestore, 'events');
+    return collectionData(eventsCollection, { idField: 'id' }) as Observable<IEvent[]>;
+  }
+
+  async deleteEvent(eventId: string): Promise<void> {
+    const eventRef = doc(this.firestore, `events/${eventId}`);
+    return deleteDoc(eventRef);
+  }
+
+  async updateEvent(eventId: string, updatedData: Partial<IEvent>): Promise<void> {
+    const eventRef = doc(this.firestore, `events/${eventId}`);
+    return updateDoc(eventRef, updatedData);
+  }
 }
